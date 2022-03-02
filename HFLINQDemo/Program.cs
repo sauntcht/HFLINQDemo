2de07c77d4708368e71e506dd8693b2d2d9c057f
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-03-02 17:03:06 -0500

    Add project files.

---
+++ HFLINQDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HFLINQDemo
+{
+    
+
+    class PrintWhenGetting
+    {
+        private int instanceNumber;
+        public int InstanceNumber
+        {
+            set { instanceNumber = value; }
+            get
+            {
+                Console.WriteLine($"Getting #{instanceNumber}");
+                return instanceNumber;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfObjects = new List<PrintWhenGetting>();
+            for (int i = 1; i < 5; i++)
+            listOfObjects.Add(new PrintWhenGetting() { InstanceNumber = i });
+
+            Console.WriteLine("Set up query");
+            var result =
+                from o in listOfObjects
+                select o.InstanceNumber;
+
+            var immediate = result.ToList();
+
+            Console.WriteLine("Run the foreach");
+            foreach (var number in immediate)
+                Console.WriteLine($"Writing #{number}");
+
+
+
+        }
+    }
+
+}
